d1546e67cf7b83d060f181be7cfcab1f8f5a8cf1
TUMU HAREESWAR <hareeswar17@gmail.com>
2026-05-17 18:17:13 +0530

    palindrome sub template

---
+++ dsa/palinfromeFromSubsr.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+void palindromicSubstrings(string& s){
+    int n = s.length();
+    
+}
+int main(){
+    string s;
+    cin >> s;
+    palindromicSubstrings(s);
+    return 0;
+}
