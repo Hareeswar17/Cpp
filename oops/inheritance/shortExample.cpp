9442323b89996fedb5b8b534ffffdf9f56da95fb
TUMU HAREESWAR <hareeswar17@gmail.com>
2026-09-21 02:52:45 +0530

    Explained about virtual & Destructor used in the code

--- oops/inheritance/shortExample.cpp
+++ oops/inheritance/shortExample.cpp
@@ -2,60 +2,76 @@
 using namespace std;
 
 // Base class
-class GameObject {
+class GameObject
+{
 protected:
     string name;
 
 public:
-    GameObject(string n) : name(n) {}
+    GameObject(string n) : name(n) {} // This way of declaring is called member initializer list
 
-    virtual void update() = 0;     // Pure virtual function
-    virtual ~GameObject() = default;
+    virtual void update() = 0; // Pure virtual function
+    // This is made because we don't want to declare any method to this function right now hence we have to declare something we can't leave it empty so we this 
+
+    virtual ~GameObject() = default; // Destructor
+    // Because the destructor is virtual, C++ looks at the actual object type
+    // And calls the Player destructor and then GameObject destructor
+    // This class can have derived classes, so destrcution should be polymorphic hence vitual keyword
 };
 
 // Base class 1
-class Renderable : virtual public GameObject {
+class Renderable : virtual public GameObject
+{
 public:
-    Renderable(string n) : GameObject(n) {}
+    Renderable(string n) : GameObject(n) {} // This is the proper way because the base class must be initialized before the constructor body runs 
+    // so we cannot do => Renderable(string n) { GameObject(n); }
 
-    void draw() {
+    void draw()
+    {
         cout << name << " is being rendered\n";
     }
 };
 
 // Base class 2
-class PhysicsBody : virtual public GameObject {
+class PhysicsBody : virtual public GameObject
+{
 public:
     PhysicsBody(string n) : GameObject(n) {}
 
-    void move() {
+    void move()
+    {
         cout << name << " is moving\n";
     }
 };
 
 // Multiple inheritance
-class Player : public Renderable, public PhysicsBody {
+class Player : public Renderable, public PhysicsBody
+{
 public:
     Player(string n)
         : GameObject(n), Renderable(n), PhysicsBody(n) {}
 
-    void update() override {
+    void update() override
+    {
         cout << name << " is updating\n";
     }
 };
 
 // Multilevel inheritance
-class AdminPlayer : public Player {
+class AdminPlayer : public Player
+{
 public:
     AdminPlayer(string n)
         : GameObject(n), Player(n) {}
 
-    void update() override {
+    void update() override
+    {
         cout << name << " is monitoring the server\n";
     }
 };
 
-int main() {
+int main()
+{
     Player player("Arthur");
     AdminPlayer admin("Neo");
 
@@ -64,7 +80,7 @@ int main() {
     player.move();
 
     // Runtime polymorphism
-    GameObject* obj = &admin;
+    GameObject *obj = &admin;
     obj->update();
 
     return 0;
